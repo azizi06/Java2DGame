f44df421c9511e3036b253491d95f063a7e99d83
Ramzi <iss.benahmed@outlook.com>
2024-05-07 13:51:30 +0200

    Changed file names

--- src/main/java/com/example/basketballgame2d/MainController.java
+++ src/main/java/com/example/basketballgame2d/MainController.java
@@ -3,7 +3,7 @@ package com.example.basketballgame2d;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class MainController {
     @FXML
     private Label welcomeText;
 
