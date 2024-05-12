3776b2f57540f3917e02fd61cdb23a30a9af7fa3
wayabali <ryadbali60@gmail.com>
2024-05-12 23:02:56 +0100

    nami wsha heda javafx

--- src/main/java/com/example/basketballgame2d/Background.java
+++ src/main/java/com/example/basketballgame2d/Background.java
@@ -1,5 +1,15 @@
 package com.example.basketballgame2d;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.StackPane;
+
 
 public class Background {
-
+    public static StackPane createImageBackground(String imagePath, double width, double height) {
+        Image image = new Image(imagePath);
+        ImageView imageView = new ImageView(image);
+        imageView.setFitWidth(width);
+        imageView.setFitHeight(height);
+        return new StackPane(imageView);
+    }
 }
