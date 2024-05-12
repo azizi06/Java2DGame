9b7ada10cfa5fba0c70f7220ec63225e192fd875
Ramzi <iss.benahmed@outlook.com>
2024-05-12 23:00:11 +0200

    Added the score functionnality (needs to be changed by an asset)

--- src/main/java/com/example/basketballgame2d/CollisionChecker.java
+++ src/main/java/com/example/basketballgame2d/CollisionChecker.java
@@ -5,10 +5,7 @@ import com.example.basketballgame2d.Cerceau.Cerceau;
 
 public class CollisionChecker {
     static boolean checkCollision(Ball ball, Cerceau cerceau) {
-        return ball.getPositionX() + ball.getBallSprite().getFitWidth() >= cerceau.getPositionX() &&
-                ball.getPositionX() <= cerceau.getPositionX() + cerceau.getCerceauSprite().getFitWidth() &&
-                ball.getPositionY() + ball.getBallSprite().getFitHeight() >= cerceau.getPositionY() &&
-                ball.getPositionY() <= cerceau.getPositionY() + cerceau.getCerceauSprite().getFitHeight();
+        return false;
     }
 
 }
